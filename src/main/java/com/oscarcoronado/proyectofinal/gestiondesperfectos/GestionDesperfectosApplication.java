1bc8bb31bd1e9e4375d14fbd3bdd9aec9f3badbc
Oscar Coronado <ocordom078@g.educaand.es>
2025-11-01 16:54:02 +0100

    estructura base del backend

---
+++ src/main/java/com/oscarcoronado/proyectofinal/gestiondesperfectos/GestionDesperfectosApplication.java
@@ -0,0 +1,13 @@
+package com.oscarcoronado.proyectofinal.gestiondesperfectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesperfectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesperfectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oscarcoronado/proyectofinal/gestiondesperfectos/GestionDesperfectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscarcoronado.proyectofinal.gestiondesperfectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesperfectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
